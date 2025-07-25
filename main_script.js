c59d3bcaf64f03b61b8d690061de9b801635ace2
LibrariumCloud <LibrarysMail@yandex.ru>
2025-07-26 02:14:34 +0300

    Update site

---
+++ main_script.js
@@ -0,0 +1,11 @@
+function OpenSite(site){
+    switch(site){
+        case "main":
+            window.location.href = "index.html";
+            break;
+        
+        case "game":
+            window.location.href = "game.html";
+            break;
+    }
+}
